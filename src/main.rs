03499b1829e29fac0c6b2bdd46e22f04d29b9957
HassanBouighamdane <hassanbouighamdane2002@gmail.com>
2025-04-10 22:14:08 +0100

    Returning sucess response

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,30 @@
 #[allow(unused_imports)]
 use std::net::TcpListener;
+use std::{io::{prelude::*, BufReader}, net::{Ipv4Addr, SocketAddrV4, TcpStream}};
+mod response;
 
+use response::{success_response};
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+    let addr=SocketAddrV4::new(Ipv4Addr::new(127, 0,0, 1), 4221);
+    let listener = TcpListener::bind(addr).unwrap();
+    //This also works 
+    //let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
      for stream in listener.incoming() {
          match stream {
          Ok(_stream) => {
-                 println!("accepted new connection");
+                 handle_connection(_stream);
              }
              Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    stream.write_all(success_response().as_bytes()).unwrap();
 }
---
+++ src/response.rs
@@ -0,0 +1,4 @@
+
+pub fn success_response()->String{
+    "HTTP/1.1 200 OK\r\n\r\n".to_string()
+}
