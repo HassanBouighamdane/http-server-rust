85f9423dc22006744aa536d0d512db87c7f6e298
HassanBouighamdane <hassanbouighamdane2002@gmail.com>
2025-04-11 17:20:18 +0100

    return not found status

--- src/response.rs
+++ src/response.rs
@@ -1,26 +1,26 @@
 
 pub fn success_response()->String{
     let status_line="HTTP/1.1 200 OK".to_string();
-    let crlt="\r\n".to_string();
+    let crlf="\r\n".to_string();
     let headers="".to_string();
     let content="".to_string();
     let response=status_line+
-        &crlt+
+        &crlf+
         &headers+
-        &crlt+
+        &crlf+
         &content;
     response
 }
 
 pub fn not_found_response()->String{
-    let status_line="HTTP/1.1 404 NOT FOUND".to_string();
-    let crlt="\r\n".to_string();
+    let status_line="HTTP/1.1 404 Not Found".to_string();
+    let crlf="\r\n".to_string();
     let headers="".to_string();
     let content="".to_string();
     let response=status_line+
-        &crlt+
+        &crlf+
         &headers+
-        &crlt+
+        &crlf+
         &content;
     response
 }
